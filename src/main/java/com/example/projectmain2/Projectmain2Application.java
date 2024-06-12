3354d37907e05a05ee8286c31bb943976ba44d55
ParkByeongsik <heaves06181223@gmail.com>
2024-06-12 22:49:03 +0900

    첫프로젝트

---
+++ src/main/java/com/example/projectmain2/Projectmain2Application.java
@@ -0,0 +1,13 @@
+package com.example.projectmain2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projectmain2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projectmain2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectmain2/Projectmain2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectmain2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projectmain2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
